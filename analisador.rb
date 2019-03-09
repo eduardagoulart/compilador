c79fe780ef1369d357ae38174c1e2b3305d754db
eduardagoulart <goularteduarda.a@gmail.com>
2019-03-09 18:04:28 -0300

    declarando os tokens aceitos pela linguagem

--- analisador.rb
+++ analisador.rb
@@ -1,10 +1,13 @@
 letras = []
 
-f = File.open('soma.txt', 'r')
+f = File.open('soma.c', 'r')
 f.each_char do |char|
   letras.append(char)
 end
-
 f.close
-letras = letras.join(" ")
+
+puts letras[0]
+letras = letras.join
 puts letras
+
+puts letras[0]
