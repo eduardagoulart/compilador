3ec4d9c3b981efb0633b877739163c30983667e9
eduardagoulart <goularteduarda.a@gmail.com>
2019-03-29 15:16:56 -0300

    analisador com automato

--- analisador.rb
+++ analisador.rb
@@ -1,121 +1,5 @@
 require_relative 'tokens'
 
-module Lexema
-  def le_arquivo
-    letras = []
-
-    f = File.open('soma.c', 'r')
-    f.each_char do |char|
-      letras << char
-    end
-    f.close
-
-    return letras
-  end
-
-  # @todo: split no código utilizar apenas espaços
-  def tokeniza
-    entrada = le_arquivo
-
-    token = []
-    reservados = []
-    entrada = entrada.join.split(/ /)
-
-
-    entrada.each do |valor|
-      if Token::MY_HASH[valor]
-        reservados << Token::MY_HASH[valor]
-        puts valor
-      elsif valor =~ /[+-]?[0-9][.][0-9]/
-        reservados << :FLOAT_CONT
-
-      elsif valor =~ /[+-]?[0-9]/
-        reservados << :INTEGER_CONST
-
-      elsif valor =~ /[a-zA-Z]/
-        reservados << :ID
-      elsif valor.match(/\s/) or valor.match(/(.|\s)*/)
-      else
-        if Token::MY_HASH[valor]
-          puts "chega aqui"
-          reservados << Token::MY_HASH[valor]
-        else
-          puts reservados
-          puts "#{valor} - Caractere inválido na linguagem"
-          exit
-        end
-      end
-    end
-    puts reservados.join(" ")
-  end
-end
-
-# include Lexema
-# tokeniza
-
-module AnalisadorLexico
-  def arquivo
-    reservados = []
-    letras = []
-    t = []
-    f = File.open('soma.c', 'r')
-
-    f.each_char do |char|
-      if char =~ /\w/
-        letras << char
-      elsif !char.match(/w/)
-        if letras.size > 0
-          # puts "char - #{char}, tipo"
-          letras = letras.join
-          t << char
-          if Token::MY_HASH[letras]
-            reservados << [Token::MY_HASH[letras], letras]
-          elsif char =~ /[+-]?[0-9][.][0-9]/
-            reservados << [:FLOAT_CONT, char]
-          elsif char =~ /[+-]?[0-9]/
-            reservados << [:INTEGER_CONST, char]
-          elsif !char.match(/\s/)
-            puts char
-            reservados << [Token::MY_HASH[t], t]
-          else
-            reservados << [:ID, letras]
-          end
-          letras = []
-        elsif char =~ /[+-]?[0-9][.][0-9]/
-          reservados << [:FLOAT_CONT, char]
-        elsif char =~ /[+-]?[0-9]/
-          reservados << [:INTEGER_CONST, char]
-        else
-          if Token::MY_HASH[char]
-            reservados << [Token::MY_HASH[char], char]
-          elsif char =~ /\s/
-          else
-            puts reservados
-            puts "#{char} - não pertence"
-            exit
-          end
-        end
-      else
-        if Token::MY_HASH[char]
-          reservados << [Token::MY_HASH[char], char]
-        elsif char =~ /\s/
-        else
-          puts reservados
-          puts "#{char} - não pertence"
-          exit
-        end
-      end
-    end
-
-    puts reservados.join.split(" ")
-    f.close
-
-  end
-end
-
-# include AnalisadorLexico
-# arquivo
-
 module Teste
   def leitura_arquivo
     letras = []
@@ -216,25 +100,30 @@ module Automata
   def state_zero
     final_result = []
     tokens = []
+    letras = []
+    t = []
     input = le_arquivo
     input.each do |char|
-      if char == ',' or char == ';' or char == '[' or char == ']' or char == '(' or char == ')' or char == '{' or char == '}'
-        final_result << Token::MY_HASH[char]
-      elsif char.match(/\w/) or char.match(/\s/)
+      if char.match(/[a-zA-Z]/)
         tokens << char
+      else
+        t << char
+        if tokens.size > 0
+          tokens = tokens.join
+          final_result << state_two(tokens)
+          tokens = []
+        elsif char == ',' or char == ';' or char == '[' or char == ']' or char == '(' or char == ')' or char == '{' or char == '}' or char == '-' or char == '+'
+          final_result << Token::MY_HASH[char]
+        elsif char =~ /([0-9])+[.]([0-9])+/
+          final_result << :FLOAT_CONT
+        elsif char.match(/[0-9]+/)
+          final_result << :INTEGER_CONST
+
+        end
       end
-      # puts tokens.length
-      tokens.join.split
-      # tokens.split(" ")
-      puts tokens
-      final_result << state_two(tokens)
-      # puts char
-
     end
-    # puts tokens
-    # puts final_result
+    puts final_result
   end
-
 end
 
 include Automata
