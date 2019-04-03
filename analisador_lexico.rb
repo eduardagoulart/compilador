3802d88499ea7a116793d95f944e7a3d3231a996
eduardagoulart <goularteduarda.a@gmail.com>
2019-04-02 22:39:24 -0300

    tabela final

--- analisador_lexico.rb
+++ analisador_lexico.rb
@@ -22,6 +22,7 @@ module Automata
   end
 
   def state_zero
+    line = 0
     final_result = []
     tokens = []
     numero = []
@@ -36,31 +37,33 @@ module Automata
         if tokens.size > 0
           tokens = tokens.join
           final_result << state_two(tokens)
-          matriz_final << [tokens, state_two(tokens)]
+          matriz_final << [tokens, state_two(tokens), line]
           tokens = []
           if char == '(' or char == '=' or char == ';' or char == '+' or char == '-'
             final_result << Token::MY_HASH[char]
-            matriz_final << [char, Token::MY_HASH[char]]
+            matriz_final << [char, Token::MY_HASH[char], line]
           end
         elsif char == ',' or char == ';' or char == '[' or char == ']' or char == '(' or char == ')' or char == '{' or char == '}' or char == '-' or char == '+' or char == '='
           if char == ';'
             numero = numero.join
             if numero.match(/[0-9]+[.][0-9]+/)
               final_result << :FLOAT_CONST
-              matriz_final << [numero, :FLOAT_CONST]
+              matriz_final << [numero, :FLOAT_CONST, line]
             elsif numero.match(/[.]([0-9])+/)
               puts "Lexema não permitido"
               exit!
             elsif numero.match(/[0-9]+/)
               final_result << :INTEGER_CONST
-              matriz_final << [numero, :INTEGER_CONST]
+              matriz_final << [numero, :INTEGER_CONST, line]
             end
             numero = []
           end
           final_result << Token::MY_HASH[char]
-          matriz_final << [char, Token::MY_HASH[char]]
+          matriz_final << [char, Token::MY_HASH[char], line]
         elsif char.match(/[0-9]/) or char == '.' or char.match(';')
           numero << char
+        elsif char.match(/\n/)
+          line += 1
         else
           if !char.match(/\s/)
             puts "Lexema #{char} não pertencente a linguagem"
@@ -69,7 +72,7 @@ module Automata
         end
       end
     end
-    puts matriz_final
+    puts matriz_final[0]
     return final_result, matriz_final
 
   end
