0b15bb8a0f5d8161451f54f77af83c2502884c32
eduardagoulart <goularteduarda.a@gmail.com>
2019-04-03 15:34:45 -0300

    função programa

--- sintaxe.rb
+++ sintaxe.rb
@@ -2,31 +2,33 @@ require_relative 'analisador_lexico'
 
 module AnalisadorSintatico
   def analise_sintatica
-    lexema, matriz = Automata::state_zero
-    lexema.each.with_index do |valor, index|
-      # puts valor, index
-      puts valor
-      programa(valor)
-    end
+    @token_entrada, matriz = Automata::state_zero
+    @index = 0
+    programa()
+    # lexema.each_with_index do |valor, index|
+    #  programa(lexema[index], index)
+    #  lexema[index]
+    # end
   end
-  def programa(token_entrada)
-    if token_entrada.to_s == "INT"
-      puts "EAE"
-      casa(token_entrada, "INT")
-      puts "opa"
+  def programa()
+    if @token_entrada[@index].to_s == "INT"
+      casa("INT")
       casa("MAIN")
-      casa("RBRACKET")
       casa("LBRACKET")
+      casa("RBRACKET")
       casa("LBRACE")
-      decl_comando()
+      # decl_comando()
       casa("RBRACE")
     end
   end
-  def casa(token_entrada, token_esperado)
-    if token_entrada.to_s == token_esperado.to_s
-      puts "to passando por aqui"
-      puts token_entrada
-      return nex = 1
+  def casa(token_esperado)
+    if @token_entrada[@index].to_s == token_esperado.to_s
+      puts "to passando por aqui #{@token_entrada[@index]} no indice #{@index}"
+      @index += 1
+      if @index < @token_entrada.length
+        return @token_entrada[@index]
+      end
+      exit!
     end
   end
 end
